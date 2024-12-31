7a318597fb82973a113fc56c11a7e5c95c339ae7
Kalyan0505 <kalyankumar1022@gmail.com>
2024-12-31 22:17:08 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Restaurant from "./components/Restaurant/Restaurant.jsx";
+
+function App() {
+  return (
+    <>
+      <Restaurant />
+    </>
+  );
+}
+
+export default App;
